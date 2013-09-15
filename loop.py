e709c3acdecadd5ef27f552d7b183a4c0d1e1054
Amos Bairn <Eylrid@Gmail.com>
2013-09-15 15:35:04 -0700

    add loop
    loop records and trashes new message on the hour, to keep the inbox down.
    It also publishes reports once every 24 hours.

---
+++ loop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from BMaggregator import *
+import time
+
+def check():
+    bma = Aggregator()
+    return bma.check()
+
+def loop():
+    while True:
+        result = check()
+        if result:
+            print 'sleeping for ' + str(result)
+            time.sleep(result)
+
+if __name__ == '__main__':
+    loop()
