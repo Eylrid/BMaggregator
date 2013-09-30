817d6787abebed3b186937d3c867b3ce2fd66e13
amos <amos@amos-Latitude-D620.(none)>
2013-09-29 17:09:22 -0700

    Catch exceptions in loop
    Catch exceptions and log exceptions. This allows loop to keep running as
    well as diagnosis errors.

--- loop.py
+++ loop.py
@@ -6,9 +6,18 @@ def check():
     bma = Aggregator()
     return bma.check(addNewMessages=False)
 
+def logerror(exception):
+    au = ApiUser()
+    au.logger.log('loop Error!!, ' + ', '.join((str(type(exception)),)+ exception.args))
+
 def loop():
     while True:
-        result = check()
+        try:
+            result = check()
+        except Exception as exception:
+            logerror(exception)
+            result = Aggregator.CHECKINTERVAL-time.time()%Aggregator.CHECKINTERVAL
+
         if result:
             print 'sleeping for ' + str(result)
             time.sleep(result)
