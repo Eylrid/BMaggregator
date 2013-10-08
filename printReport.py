e0562a58ce570158c0a164308f7030c42bd3004f
Amos Bairn <Eylrid@Gmail.com>
2013-10-08 11:45:15 -0400

    unicode support in printReport

--- printReport.py
+++ printReport.py
@@ -5,8 +5,8 @@ from BMaggregator import *
 args = sys.argv[1:]
 bma = Aggregator()
 if not args or '-main' in args:
-    print bma.getMainReport()
+    print bma.getMainReport().encode('utf-8')
 if '-chans' in args:
-    print bma.getChanSubjectReport()
+    print bma.getChanSubjectReport().encode('utf-8')
 if '-broadcasts' in args:
-    print bma.getBroadcastSubjectReport()
+    print bma.getBroadcastSubjectReport().encode('utf-8')
